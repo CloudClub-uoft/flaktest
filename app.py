1ea5234ee787dd1271d3d0444f4813eea0a072c3
Harsimrat <singh21.h@gmail.com>
2020-10-31 20:05:06 -0400

    fix requremeitns

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ def add():
     db.Put(time.time(), data)
     return redirect("/")
 
-
-
-app.run();
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
