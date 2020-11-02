cb2604ba2b389318d0afcb04c3a079f8c6e80d66
Harsimrat <singh21.h@gmail.com>
2020-11-02 16:12:35 -0500

    use mongo

--- app.py
+++ app.py
@@ -2,32 +2,45 @@ from flask import Flask, request, render_template,redirect, url_for
 import time
 app = Flask(__name__)
 
-import easydbio
+import pymongo
+import time, threading
 
-db = easydbio.DB({
-  "database": "4a4e6a86-1fbe-45d1-a3c8-6fefa2b6e499",
-  "token": "c06a2b02-898d-4dce-beb9-61db4ed3177a"
-})
+#import connection strings
+from const import *
 
+#connect to mongoDB client
+client = pymongo.MongoClient(string) #insert connection string
+db2 = client.get_database('CLOUDCLUSTER')
+p = db2.test_collection
+
+#default route
 @app.route('/')
 def hello_world():
-    return render_template("index.html", items = db.List().items())
+    return render_template("index.html", items = list(p.find()))
     
-
+#add an item
 @app.route('/add')
-def add():
-    
+def add():    
     data = request.args.get('data')
-    db.Put(time.time(), data)
+    post = {data: data}
+    #query for adding an item to MongoDb collection
+    p.insert_one(post)
     return redirect("/")
 
+#delete all items
 @app.route('/deleteAll')
 def deleteAll():
-    keys = db.List().keys()
-    for key in keys:
-        db.Delete(key)
+    #query for removing all items from MongoDB collection
+    p.remove({})
     return redirect("/")
+
+#remove items after every 1800 seconds
+def foo():
+    p.remove({})
+    threading.Timer(1800, foo).start()
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
+    foo()
     app.run(threaded=True, port=5000)
